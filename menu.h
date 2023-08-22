e09d2fc00bafa631fcfb4aa877a19f30e8ab6865
sergxlove <sergxlove@mail.ru>
2023-08-22 11:51:26 +0600

    finaly_version

--- menu.h
+++ menu.h
@@ -1,13 +1,18 @@
 #pragma once
 #ifndef _MENU_H_
 #define _MENU_H_
-void info_console();
-void done(std::string name_file);
-void done(std::string name_first_file, std::string name_second_file);
-void done(std::string name_first_file, std::string name_second_file, std::string name_third_file);
-void error(std::string name_file);
-void error(std::string name_first_file, std::string name_second_file);
-void error(std::string name_first_file, std::string name_second_file, std::string name_third_file);
-void print_line();
+class menu
+{
+public:
+	void info_console();
+	void done(std::string name_file);
+	void done(std::string name_first_file, std::string name_second_file);
+	void done(std::string name_first_file, std::string name_second_file, std::string name_third_file);
+	void error(std::string name_file);
+	void error(std::string name_first_file, std::string name_second_file);
+	void error(std::string name_first_file, std::string name_second_file, std::string name_third_file);
+	void print_line();
+private:
+};
 #endif // !_MENU_H_
 
